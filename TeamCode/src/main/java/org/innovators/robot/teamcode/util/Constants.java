6a67d8fdd3685c0b4309204fcd6e6dbd8ccb648a
Serat Ippili <seratbabu@hotmail.com>
2025-01-16 19:02:14 -0500

    Initial Version

---
+++ TeamCode/src/main/java/org/innovators/robot/teamcode/util/Constants.java
@@ -0,0 +1,9 @@
+package org.innovators.robot.teamcode.util;
+public class Constants {
+    public static final double DRIVE_SPEED = 0.5;
+    public static final double TURN_SPEED = 0.3;
+    public static final double SERVO_POWER_UP = 1.0;
+    public static final double SERVO_POWER_MID = 0.5;
+    public static final double SERVO_POWER_DOWN = 0.0;
+}
+
